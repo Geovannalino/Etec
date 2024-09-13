c4b889e3dcd0b8c265bf3ac67197985385a9644b
Geovannalino <geovannadasilvalino@gmail.com>
2024-09-12 21:27:36 -0300

    Add files via upload

---
+++ exercicos sala/Km.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Km {
+	public static void main (String args[] ) {
+			Scanner ler = new Scanner (System.in);
+			double km, metro;
+			
+			System.out.println("digite a quantidade de metros");
+			metro = ler.nextInt();
+			ler.close();
+			
+			km = metro / 1000;
+			
+			System.out.println("o número de quilometros é " + km);
+		}
+	}
+
