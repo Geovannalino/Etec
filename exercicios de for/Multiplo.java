d11553af35fd1d0452f2745b8851495011e00858
Geovannalino <geovannadasilvalino@gmail.com>
2024-09-19 20:49:39 -0300

    Add files via upload

---
+++ exercicios de for/Multiplo.java
@@ -0,0 +1,20 @@
+
+public class Multiplo {
+	public static void main(String[] args) {
+		
+		  for(int i=1;i<=100;i++) {
+			
+			System.out.println(i);	 
+			
+			if( i % 10 == 0){
+		     System.out.println( "Multiplo de 10:"+i);
+			
+				
+			
+			 
+			}
+	}
+	}
+
+}
+
