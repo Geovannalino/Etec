b33fb40fda4f90627be1a27475a2429dc84afa76
Geovannalino <geovannadasilvalino@gmail.com>
2024-09-22 17:19:01 -0300

    Add files via upload

---
+++ do while/num.java
@@ -0,0 +1,14 @@
+public class num {
+	public static void main(String[] args) {
+		    	int i=0;
+		    	System.out.println("Os numero pares são:");
+		   	   do {
+		   		  System.out.println(i);
+		   		  i+=2;
+		   		  }   
+		   	   while(i<=500);
+		   	   
+		  }
+		}
+
+
